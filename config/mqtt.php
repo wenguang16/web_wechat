de19709c4909d0e54dfe8bdaec96d83cea165b77
陈文广 <chenwg@cecjiutian.com>
2025-06-23 17:08:22 +0800

    test

--- config/mqtt.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2018/8/20
- * Time: 14:24
- */
--- public/info.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 2018/8/20
- * Time: 11:29
- */
-phpinfo();
