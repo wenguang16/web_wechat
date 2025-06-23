366fa424dc1b7155b67b9b5bd0e367d803e99ac1
陈文广 <chenwg@cecjiutian.com>
2025-06-23 18:01:08 +0800

    test

---
+++ cwg-picture/src/main/java/com/cwg/picture/CwgPictureApplication.java
@@ -0,0 +1,13 @@
+package com.cwg.picture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwgPictureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CwgPictureApplication.class, args);
+    }
+
+}
---
+++ cwg-picture/src/test/java/com/cwg/picture/CwgPictureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwg.picture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwgPictureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
